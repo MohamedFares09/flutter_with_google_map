7383910413e03d121d7ec5919c6c90d947036ce7
MohamedFares09 <mofares010@gmail.com>
2026-01-08 22:41:41 +0200

    updata main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_with_google_maps/firebase_options.dart';
-import 'package:flutter_with_google_maps/google_ex.dart';
+import 'package:flutter_with_google_maps/google_maps_view.dart';
 
 
 void main() { 
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(const RouteTrakerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RouteTrakerApp extends StatelessWidget {
+  const RouteTrakerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: GoogleEx());
+    return MaterialApp(home: GoogleMapsView());
   }
 }
